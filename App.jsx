631a493f8556b5b1e7ffd51bcec8e08927a87afd
tarris1@hotmail.com <tarris1@hotmail.com>
2019-11-30 10:02:00 +0100

    Fixed naming of filesave

--- App.jsx
+++ App.jsx
@@ -11,11 +11,11 @@ var fileSaver = require('file-saver');
 function transferToDict(id) {
 	var dictionary = data['drugs'][id];
 	return {
-		id:id,
-		names:dictionary['name'],
-		category:dictionary['category'],
-		firm:dictionary['misc'],
-		indications:dictionary['disease']
+		id: id,
+		names: dictionary['name'],
+		category: dictionary['category'],
+		firm: dictionary['misc'],
+		indications: dictionary['disease']
 	}
 }
 
@@ -70,10 +70,7 @@ class App extends React.Component {
 	   
    }
 	
-	handleClick(event){
-		var file = event.target.files[0];
-		console.log(file);
-	}
+
 	
 	hideTable(event){ //Hides the table
 		if(this.state.hideDatabase == "Unhide database") {
@@ -83,8 +80,14 @@ class App extends React.Component {
 	
 	saveData(event){ //Saves the data with the name determined by this.state.saveName in the label, maybe save with the name of search or a generic name if no search input
 		var blob = new Blob([JSON.stringify(this.state.data)], {type: "octet/stream"});
+		const saveToName = this.state.saveName;
+		const searchName = this.state.search;
 		//Add an if function that checks if file exists in cwd or not, show error if it exists
-		fileSaver.saveAs(blob, this.state.saveName+".json");
+		if (saveToName.length>0 && searchName.length>0){
+			fileSaver.saveAs(blob, saveToName+".json")
+		} else if (saveToName.length==0) 
+			{fileSaver.saveAs(blob, searchName+".json")}
+			else {fileSaver.saveAs(blog, "na.json")};
 	}
 	
 	changeSaveDataName(event){this.setState({saveName:event.target.value})} //Changes the save data label - fix so that it works for any label
@@ -100,16 +103,9 @@ class App extends React.Component {
 				<tbody>
 					
 					<tr>
+						<td><label>Search: <input type="text" name = "search" value = {this.state.search} onChange = {this.searchResults}/></label></td>
 						<td><label>Save data as: <input type="text" name = "saveData" value = {this.state.saveName} onChange = {this.changeSaveDataName}/></label></td>
 						<td><button onClick = {this.saveData}>Click to save</button></td>
-					</tr>
-					
-					<tr>
-						<td><input type = "file" id = "file" ref="fileUploader" onChange = {this.handleClick} /></td>
-					</tr>
-					
-					<tr>
-						<td><label>Search: <input type="text" name = "search" value = {this.state.search} onChange = {this.searchResults}/></label></td>
 					</tr>
 			
 				</tbody>
@@ -215,11 +211,11 @@ class DrugTableRow extends React.Component {
    render() {
       return (
          <tr>
-            <td >{this.props.data.id}</td>
-            <td >{this.props.data.names}</td>
-            <td >{this.props.data.indications}</td>
-			<td >{this.props.data.category}</td>
-			<td >{this.props.data.firm}</td>
+            <td> {this.props.data.id}</td>
+            <td> {this.props.data.names}</td>
+            <td> {this.props.data.indications}</td>
+			<td> {this.props.data.category}</td>
+			<td> {this.props.data.firm}</td>
         </tr>	
       );
    }
