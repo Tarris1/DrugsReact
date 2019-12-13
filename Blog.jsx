ec959827155b445ab94697ae227094fbaf631679
tarris1@hotmail.com <tarris1@hotmail.com>
2019-12-13 13:09:34 +0100

    Fixed blog entry

--- Blog.jsx
+++ Blog.jsx
@@ -1,37 +1,32 @@
 import React from 'react';
 
-
-
 class Blog extends React.Component {
 	render() {
 		return (
 			<div>
 				<h1>Welcome to my blog</h1>
-				<p>Introduction</p>
+				<p>In this blog I will give overviews and analysis on the various drugs and companies that I encounter in my studies.</p>
 				
-				{this.props.data.map((blog, blogNumber) => <BlogEntry key = {blogNumber} data= {blog}/>)}</div>
+				{this.props.data.map((blog, blogNumber) => <BlogEntry key={blogNumber} data={blog}/>)}</div>
 		)
 	}
 }
 //https://dev.to/zeyadetman/how-to-pass-state-between-components-in-reactjs-2pg1
 
 class BlogEntry extends React.Component {
-	//Creates each blog entry
 	render() {
 		return (
 			<table>
 				<tbody>
-					{/*<tr><th>{"#"+this.props.data.id+": "+this.props.data.title}</th></tr>
-					<tr><td>{this.props.data.text}</td></tr>*/}
+					<tr id="blogTitle"><td>{"#" + this.props.data.id + ": " + this.props.data.title}</td></tr>
 					<tr><td dangerouslySetInnerHTML={ { __html: this.props.data.text } }></td></tr>
-					<tr><td>{"Author: "+this.props.data.author}</td></tr>
-					<tr><td>{"Date: "+this.props.data.date}</td></tr>
+					{/*<tr><td>{"Author: " + this.props.data.author}</td></tr>*/}
+					<tr><td>{"Date: " + this.props.data.date}</td></tr>
+					<tr><td>{"Last edited: " + this.props.data.lastEdited}</td></tr>
 				</tbody>
 			</table>
 		);
 	}	
 }
-
-
 
 export default Blog;
